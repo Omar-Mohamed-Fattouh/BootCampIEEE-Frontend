aea710f24d7abf25da1e2dad8102feec393e2f16
Omar Mohamed Fattouh <162017850+Omar-Mohamed-Fattouh@users.noreply.github.com>
2025-03-01 17:19:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function showSection(sectionId) {
+  document.querySelectorAll("section").forEach((section) => {
+    section.style.display = "none";
+  });
+  document.getElementById(sectionId).style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  showSection("intro");
+});
+
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const mainContent = document.getElementById("main-content");
+  const toggleButton = document.getElementById("toggle-sidebar");
+  sidebar.classList.toggle("collapsed");
+  mainContent.classList.toggle("expanded");
+  toggleButton.classList.toggle("collapsed");
+}
